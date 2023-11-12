d42df81ce93827dbaabd794e1566694c66f6e7f1
Amadou Moussa Ibrahim <ibrahim.amadou@optima-esn.com>
2023-11-12 15:50:21 +0100

    implementing category views with viewsets

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryView(viewsets.ViewSet):
+
+    # listing all category
+
+    queryset = Category.objects.all()
+    
+    def list(self, request):
+
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
